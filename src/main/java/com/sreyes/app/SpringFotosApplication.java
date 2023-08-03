09df54e60c1faea30e765e22200d975611dd15b0
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2023-08-03 18:54:49 -0500

    primer commit, creando respositorio

---
+++ src/main/java/com/sreyes/app/SpringFotosApplication.java
@@ -0,0 +1,13 @@
+package com.sreyes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreyes/app/SpringFotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreyes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
